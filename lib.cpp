f92caea2be5581979af71a3019bf732cc403e11b
FedericoCarrara02 <91536069+FedericoCarrara02@users.noreply.github.com>
2021-12-09 13:51:57 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,24 @@
+#include "lib.h"
+
+float max_array(float numero[], unsigned int n) {
+    float actual = 0;
+    float prec = 0;
+    float res = 0;
+    for (int i = 0; i<n; i++) {
+        actual = numero[i];
+        if (i == 0) {
+            prec = actual;
+        }
+        else {
+            prec = res;
+        }
+        if(actual >= prec) {
+            res = actual;
+        }
+        else {
+            res = prec;
+        }
+    }
+    return res;
+}
 
