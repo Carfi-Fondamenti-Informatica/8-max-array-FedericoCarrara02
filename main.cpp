386710f497fee3257f5d6727a2a78f1d83aac3d5
FedericoCarrara02 <91536069+FedericoCarrara02@users.noreply.github.com>
2021-12-09 13:52:18 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
-  return 0;
+using namespace std;
+
+
+int main() {
+    int n = 0;
+
+    cin >> n;
+    float num_list[n];
+
+    for (int i = 0; i < n; ++i) {
+        float a;
+        cin >> a;
+        num_list[i] = a;
+
+    }
+    cout << max_array(num_list, n) << endl;
+    return 0;
 }
+
